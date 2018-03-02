748964a1b1b72b2353afccbba9a86db6edc26b61
Rostyslav Sova <sova.rostislav@gmail.com>
2018-03-02 07:38:27 +0000

    formating

--- js/index.js
+++ js/index.js
@@ -1,10 +1,9 @@
 function fibRecursion(n) {
-    return n <= 1 ? n : fibRecursion(n - 1) + fibRecursion(n - 2); 
+    return n <= 1 ? n : fibRecursion(n - 1) + fibRecursion(n - 2);
 }
 
-
 function fibCycle(n) {
-    let a =1;
+    let a = 1;
     let b = 1;
     for (let i = 3; i <= n; i++) {
         let c = a + b;
@@ -14,36 +13,35 @@ function fibCycle(n) {
     return b;
 }
 
-let arrOfFib = [0,1,1];
+let arrOfFib = [0, 1, 1];
 
 function fibArray(n) {
-    if (n in arrOfFib){
+    if (n in arrOfFib) {
         return arrOfFib[n];
-    } 
+    }
     arrOfFib[n] = fibArray(n - 1) + fibArray(n - 2);
     return arrOfFib[n];
 }
 
-
 let work = true
-while(work === true){
-let n   =  parseInt(prompt('Enter a positive integer number'));
-let alg = parseInt(prompt('Choose algorithm: Enter 1 - Recursion, Enter 2 - Cycle, Enter 3 - With Array'));
-let answer = 0;
-switch (alg) {
-    case 1:
-        answer = fibRecursion(n);
-        break;
-    case 2:
-        answer = fibCycle(n);
-        break;
-    case 3:
-        answer = fibArray(n);
-        break;
-    
-    default:
-        alert('You enter a wrong number');
-}
-document.write(n +' numer of fibonacci is ' + answer+'<br>');
-work = setTimeout((confirm('Want to continue?')), 10000);
+while (work === true) {
+    let n = parseInt(prompt('Enter a positive integer number'));
+    let alg = parseInt(prompt('Choose algorithm: Enter 1 - Recursion, Enter 2 - Cycle, Enter 3 - With Array'));
+    let answer = 0;
+    switch (alg) {
+        case 1:
+            answer = fibRecursion(n);
+            break;
+        case 2:
+            answer = fibCycle(n);
+            break;
+        case 3:
+            answer = fibArray(n);
+            break;
+
+        default:
+            alert('You enter a wrong number');
+    }
+    document.write(n + ' numer of fibonacci is ' + answer + '<br>');
+    work = setTimeout((confirm('Want to continue?')), 10000);
 }
